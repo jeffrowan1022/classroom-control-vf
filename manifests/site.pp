67e0efe24c5f79a6bcac063cf30e22f031e00396
jeffrowan1022 <jeff_rowan@abercrombie.com>
2016-01-11 17:37:50 -0500

    Lab7.2

--- manifests/site.pp
+++ manifests/site.pp
@@ -50,11 +50,17 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
-  file { '/etc/motd':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "Making changes to a file! \n",
-    }
+ # file { '/etc/motd':
+  #  ensure  => file,
+  #  owner   => 'root',
+  #  group   => 'root',
+  # mode    => '0644',
+  #  content => "Making changes to a file! \n",
+  # }
+#}
+
+exec { "cowsay "Welcome to ${::hostname}!' > /etc/motd":
+  path    => '/usr/bin:/usr/local/bin',
+  creates => '/etc/motd',
+  }
 }
