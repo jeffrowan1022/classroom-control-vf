49dd5e731f4f59f9605c91716d82d162ee391989
jeffrowan1022 <jeff_rowan@abercrombie.com>
2016-01-11 17:20:12 -0500

    Managing /etc/motd
    Lab 7.1 - Puppet Fundamentals Class - Add a file with some content!

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,4 +49,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root'
+    mode    => '0644',
+    content => "Making changes to a file! \n",
+    }
+    }
 }
