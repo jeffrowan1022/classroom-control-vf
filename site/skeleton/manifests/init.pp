474db9e9b4d1085ea0d4aef2491b4ae6daa7e771
jeffrowan1022 <jeff_rowan@abercrombie.com>
2016-01-12 13:30:39 -0500

    Lab9.3.2

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,9 +1,17 @@
 class skeleton {
-  file { 'etc/skel':
+
+  file { '/etc/skel':
     ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
-  file { 'etc/skel/.bashrc':
+  
+  file { '/etc/skel/.bashrc':
     ensure => file,
+    owner  => 'root'
+    group  => 'root'
+    mode   => '0644',
     source => 'puppet:///modules/skeleton/bashrc',
   }
 }
