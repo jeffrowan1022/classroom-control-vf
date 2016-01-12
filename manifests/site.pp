a9d22e4ab2b0768d5fc642e3f901ffb6f899336c
jeffrowan1022 <jeff_rowan@abercrombie.com>
2016-01-12 12:55:37 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -67,8 +67,11 @@ exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
   }
 
 
-host { 'testing.puppetlabs.vm':
- ensure  => present,
- ip      => '127.0.0.1',
- }
+#host { 'testing.puppetlabs.vm':
+# ensure  => present,
+# ip      => '127.0.0.1',
+# }
+ 
+ include users
+ 
 }
