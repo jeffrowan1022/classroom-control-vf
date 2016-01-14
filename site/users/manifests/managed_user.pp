fde34593073a78905b99099bbd78777ec67fff10
jeffrowan1022 <jeff_rowan@abercrombie.com>
2016-01-14 10:28:49 -0500

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,10 +1,12 @@
-define users::managed_user {
+define users::managed_user (
+  $grpoup = $title,
+) {  
  user { $title:
    ensure => present,
   }
  file { ["/home/${title}", "/home/.ssh"]:
    ensure => directory,
    owner  => $title,
-   group  => $title,
+   group  => $group,
   }
 }
